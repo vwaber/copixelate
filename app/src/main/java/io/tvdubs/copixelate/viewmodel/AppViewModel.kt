938f45eaeacecbe7204cbfe26c72e1d4ca237a93
vwaber <vwaber@gmail.com>
2022-08-14 22:22:36 -0700

    Add initial drawing and palette implementation

--- app/src/main/java/io/tvdubs/copixelate/viewmodel/AppViewModel.kt
+++ app/src/main/java/io/tvdubs/copixelate/viewmodel/AppViewModel.kt
@@ -1,12 +1,8 @@
 package io.tvdubs.copixelate.viewmodel
 
-import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
-import java.util.*
+import io.tvdubs.copixelate.ui.art.Drawing
 
 class AppViewModel() : ViewModel() {
-    // Small random bitmap for demo purposes
-    private val emptyArray = IntArray(16) { Random().nextInt() }
-
-    val bitmap: Bitmap = Bitmap.createBitmap(emptyArray, 4, 4, Bitmap.Config.ARGB_8888)
+    val drawing: Drawing = Drawing()
 }
